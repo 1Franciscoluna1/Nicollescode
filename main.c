cc5875bdd2d44cc55de973df248be2e24d763ce0
Jose Francisco Francisco <>
2021-06-06 05:14:06 +0000

    Ahora si ya jala lo que me paso nicolle

--- main.c
+++ main.c
@@ -1,42 +1,44 @@
 //programa donde realices una estructura de un alumno nombre, telefono, sexo, edad y altura 
 //#include <conio.h>
 #include<stdio.h>
-struct dato
+typedef struct 
 {
 	char nombre[20],tel[10],s;
 	int edad;
 	float alt;
-};
-typedef dato d_alumno;
-main()
+}dato;
+
+
+int main()
 {
+  dato alumno[5];
 	int opc;
 
-
-	d_alumno alumno;
 	do 
 	{
 	
 	printf("elige una opcion\n\t1. iniciar menu\t\n2. salir");
-	scanf("%i",opc);
+	scanf("%i",&opc);
 	switch(opc)
 	{
     case 1:
     	
 		
 	printf("\n\tescribie tu nombre: ");
-	fflush (stdin); gets(alumno.nombre);
+	//fflush (stdin); 
+	scanf(" %[^\n]s",alumno[1].nombre);
 	printf("\n\tescribe tu telefono: ");
-	fflush(stdin); gets(alumno.tel);
+	//fflush(stdin); 
+	scanf(" %[^\n]s",alumno[1].tel);
 	printf("\n\tescribe tu altura: ");
-	scanf("%f",& alumno.alt);
+	scanf("%f",& alumno[1].alt);
 	printf("\n\tEscribe tu sexo: ");
-	scanf("%s",& alumno.s);
+	scanf("%s",& alumno[1].s);
 	printf("\n\tescribe tu edad: ");
-	scanf("%i",& alumno.edad);
+	scanf("%i",& alumno[1].edad);
 	printf("\n\tlos datos del alumno son: ");
-	printf("\n\tnombre: %s", alumno.nombre);
-	printf("\n\tedad: %i", alumno.edad);
+	printf("\n\tnombre: %s", alumno[1].nombre);
+	printf("\n\tedad: %i\n\n", alumno[1].edad);
 
 	break;
 	case 2:
